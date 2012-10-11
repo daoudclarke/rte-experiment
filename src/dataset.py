156d8cdc8342e25a42835be1515c14851c663ffb
Daoud Clarke <daoud.clarke@gmail.com>
2012-10-11 17:14:46 +0100

    Refactor to pass in a list of files

--- src/dataset.py
+++ src/dataset.py
@@ -42,62 +42,55 @@ def get_entities(sentence):
     entities = set([x[0] for x in parse.pos() if x[-1] == 'NE'])
     return entities
 
-def process_documents(path):
+def process_documents(file_paths):
     num_positive = 0
     num_negative = 0
     
-    subdirs = os.listdir(path)
-    subdirs.sort()
     output_file = open('pairs.csv','w')
     fieldnames = ["path", "text", "hypothesis", "entails"]
     output = DictWriter(output_file, fieldnames)
-    for s in subdirs:
-        new_path = os.path.join(path,s)
-        files = os.listdir(new_path)
-        files.sort()
-        for f in files:
-            file_path = os.path.join(new_path,f)
-            print file_path
-            file_ = open(file_path)
-            headline = file_.readline()[:-1]
-            body = file_.read()
-            sentences = tokenize.sent_tokenize(body)
-            old_entities = sentence_entities = get_entities(sentences[0])
-            if valid_sentence(headline) and valid_sentence(sentences[0]):
-                headline_entities = get_entities(headline)
-                if len(headline_entities & sentence_entities) > 0:
-                    print "-- Entailing --"
-                    print sentences[0]
-                    print headline
-                    print
-                    output.writerow({"path":file_path,
-                                     "text":headline,
-                                     "hypothesis":sentences[0],
-                                     "entails":1})
-                    num_positive += 1
-            if num_positive >= max_pairs and num_negative >= max_pairs:
-                output_file.close()
-                return
-            if num_positive < num_negative:
+    for file_path in file_paths:
+        print file_path
+        file_ = open(file_path)
+        headline = file_.readline()[:-1]
+        body = file_.read()
+        sentences = tokenize.sent_tokenize(body)
+        old_entities = sentence_entities = get_entities(sentences[0])
+        if valid_sentence(headline) and valid_sentence(sentences[0]):
+            headline_entities = get_entities(headline)
+            if len(headline_entities & sentence_entities) > 0:
+                print "-- Entailing --"
+                print sentences[0]
+                print headline
+                print
+                output.writerow({"path":file_path,
+                                 "text":headline,
+                                 "hypothesis":sentences[0],
+                                 "entails":1})
+                num_positive += 1
+        if num_positive >= max_pairs and num_negative >= max_pairs:
+            output_file.close()
+            return
+        if num_positive < num_negative:
+            continue
+        for i in range(1, len(sentences) - 1):
+            text = sentences[i]
+            hypothesis = sentences[i + 1]
+            text_entities = old_entities
+            old_entities = hypothesis_entities = get_entities(hypothesis)
+            if not valid_sentence(text) or not valid_sentence(hypothesis):
                 continue
-            for i in range(1, len(sentences) - 1):
-                text = sentences[i]
-                hypothesis = sentences[i + 1]
-                text_entities = old_entities
-                old_entities = hypothesis_entities = get_entities(hypothesis)
-                if not valid_sentence(text) or not valid_sentence(hypothesis):
-                    continue
-                if len(hypothesis_entities & text_entities) > 0:
-                    print "-- Non entailing --"
-                    print text
-                    print hypothesis
-                    output.writerow({"path":file_path,
-                                     "text":text,
-                                     "hypothesis":hypothesis,
-                                     "entails":0})
-                    num_negative += 1
-                    if num_positive < num_negative:
-                        break
+            if len(hypothesis_entities & text_entities) > 0:
+                print "-- Non entailing --"
+                print text
+                print hypothesis
+                output.writerow({"path":file_path,
+                                 "text":text,
+                                 "hypothesis":hypothesis,
+                                 "entails":0})
+                num_negative += 1
+                if num_positive < num_negative:
+                    break
 
     output_file.close()
 
@@ -115,5 +108,5 @@ def get_files(path):
 
 if __name__ == "__main__":
     global data_dir
-    print get_files(data_dir)[:100]
-    process_documents(data_dir)
+    files = get_files(data_dir)[:100]
+    process_documents(files)
